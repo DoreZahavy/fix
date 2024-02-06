e2549b2151e40283a2a6fea678b8898538c74dfc
Dore Zahavy <dorezahavy@gmail.com>
2024-02-07 00:59:36 +0200

    Connect test overlay inputs and cmp data with 2-way data binding

--- src/app/pages/website-editor/editable-cmps/editable-txt/editable-txt.component.ts
+++ src/app/pages/website-editor/editable-cmps/editable-txt/editable-txt.component.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { Cmp } from '../../../../models/website.model';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'editable-txt',
   standalone: true,
-  imports: [OverlayModule,CommonModule],
+  imports: [OverlayModule,CommonModule,FormsModule],
   templateUrl: './editable-txt.component.html',
   styleUrl: './editable-txt.component.scss'
 })
